a33210c913fa43daa6953282e5dfa55368b38029
katsuyorii <katsuyoriiii@gmail.com>
2025-03-08 13:27:07 +0300

    Create user add and user response schemas, include users router

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
 
+from users.routers import users_router
+
 
 app = FastAPI()
+
+app.include_router(users_router)
---
+++ backend/users/schemas.py
@@ -0,0 +1,78 @@
+import re
+
+from fastapi import HTTPException
+from pydantic import BaseModel, ConfigDict, EmailStr, Field, field_validator
+
+from datetime import datetime, date
+
+from .models import UserRole
+
+
+REGEX_USERNAME = re.compile(r"^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$")
+
+REGEX_NAMES = re.compile(r"^[a-zA-Zа-яА-Я ,.'-]+$")
+
+REGEX_PASSWORD = re.compile(r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")
+
+class BaseORMSchema(BaseModel):
+    ''' Base schema class for convert to json (ORM) '''
+    config = ConfigDict(from_attributes=True)
+
+class UserResponseSchema(BaseORMSchema):
+    id: int
+    email: EmailStr
+    username: str
+    role: UserRole
+    first_name: str | None
+    last_name: str | None
+    date_of_birth: date | None
+    is_active: bool
+    created_at: datetime
+
+class UserCreateSchema(BaseModel):
+    email: EmailStr
+    username: str = Field(max_length=128)
+    password: str = Field(min_length=8)
+    first_name: str | None = Field(default=None, max_length=128)
+    last_name: str | None = Field(default=None, max_length=128)
+    date_of_birth: date | None = Field(default=None)
+
+    @field_validator(field='username', mode='before')
+    @classmethod
+    def validate_username(cls, value):
+        if not REGEX_USERNAME.match(value):
+            raise HTTPException(
+                status_code=422,
+                detail="Username должен быть от 8 до 128 символов, может содержать только латинские буквы, цифры, '_', '.', и не может начинаться или заканчиваться на '_' или '.'"
+            )
+        return value
+    
+    @field_validator(field='first_name', mode='before')
+    @classmethod
+    def validate_first_name(cls, value):
+        if not REGEX_NAMES.match(value):
+            raise HTTPException(
+                status_code=422,
+                detail="Имя и фамилия могут содержать только буквы, пробелы, апострофы, дефисы и точки. Цифры и спецсимволы запрещены."
+            )
+        return value
+    
+    @field_validator(field='last_name', mode='before')
+    @classmethod
+    def validate_last_name(cls, value):
+        if not REGEX_NAMES.match(value):
+            raise HTTPException(
+                status_code=422,
+                detail="Имя и фамилия могут содержать только буквы, пробелы, апострофы, дефисы и точки. Цифры и спецсимволы запрещены."
+            )
+        return value
+    
+    field_validator(field='password', mode='before')
+    @classmethod
+    def validate_password(cls, value):
+        if not REGEX_PASSWORD.match(value):
+            raise HTTPException(
+                status_code=422,
+                detail="Пароль должен содержать минимум 8 символов, хотя бы одну заглавную букву, одну строчную букву, одну цифру и один специальный символ (#?!@$%^&*-)."
+            )
+        return value
