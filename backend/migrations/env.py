8e0b59d4f455e6e2e258d926f9c4819e777de291
katsuyorii <katsuyoriiii@gmail.com>
2025-03-08 12:27:47 +0300

    Create user model and migrate

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -10,6 +10,8 @@ from alembic import context
 from src.database import BaseModel
 from src.config import settings
 
+from users.models import UserModel
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
